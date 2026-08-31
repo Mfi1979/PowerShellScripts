adfe69fa72d291acafe1df2c76111a2ee4062a77
Mfi1979 <156626365+Mfi1979@users.noreply.github.com>
2026-08-31 11:28:47 +0200

    Create AD_RenameComputerAllow.ps1
    Anpassung der Target OU und Groupname notwendig

---
+++ AD_RenameComputerAllow.ps1
@@ -0,0 +1,30 @@
+Import-Module ActiveDirectory
+
+$TargetOU_DN = "OU=Rechner,DC=rika,DC=net"
+$GroupName   = "ADS-SEC-Clients-ManageComputers"
+
+$TargetOU = [ADSI]"LDAP://$TargetOU_DN"
+$ACL = $TargetOU.ObjectSecurity
+$GroupSID = (Get-ADGroup -Identity $GroupName).SID.Value
+$IdentityRef = [System.Security.Principal.SecurityIdentifier]::new($GroupSID)
+
+$ComputerClassGUID = [System.Guid]::Parse("BF967A86-0DE6-11D0-A285-00AA003049E2")
+$InheritanceAll     = [System.DirectoryServices.ActiveDirectorySecurityInheritance]::All
+
+# Notwendig: CreateChild, DeleteChild UND WriteProperty auf Computer-Objekten
+$Rights = [System.DirectoryServices.ActiveDirectoryRights]::CreateChild `
+    -bor [System.DirectoryServices.ActiveDirectoryRights]::DeleteChild `
+    -bor [System.DirectoryServices.ActiveDirectoryRights]::WriteProperty `
+    -bor [System.DirectoryServices.ActiveDirectoryRights]::Delete
+
+$AccessRule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new(
+    $IdentityRef,
+    $Rights,
+    [System.Security.AccessControl.AccessControlType]::Allow,
+    $ComputerClassGUID,
+    $InheritanceAll
+)
+
+$ACL.AddAccessRule($AccessRule)
+$TargetOU.CommitChanges()
+Write-Host "Rechte für Computer-Rename erfolgreich erweitert!" -ForegroundColor Green
